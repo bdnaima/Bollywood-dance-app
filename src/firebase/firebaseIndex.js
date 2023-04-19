e3c2e44326faf95c8342a0b4c60c5d2a051a42ca
Naima Maria Malik <Naima@Saads-MBP.lan>
2023-04-19 12:06:52 +0200

    Update packages and Firebase config

--- src/firebase/firebaseIndex.js
+++ src/firebase/firebaseIndex.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firebase-firestore";
 
 const app = firebase.initializeApp({
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyAQ4pzhU2vbwoWD1uk3eT9TIe75qNlxFxY",
+  authDomain: "dance-app-9c409.firebaseapp.com",
+  projectId: "dance-app-9c409",
+  storageBucket: "dance-app-9c409.appspot.com",
+  messagingSenderId: "524775154487",
+  appId: "1:524775154487:web:ac4b45eb8e49d9c21db0b2",
 });
 
 export const auth = app.auth();
 export const db = app.firestore();
-export const FieldValue = firebase.firestore.FieldValue
+export const FieldValue = firebase.firestore.FieldValue;
